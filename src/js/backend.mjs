dfa996642aed8d1ac27970ec76ca9e37cfee711b
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-02-17 20:15:03 +0100

    commit 1 TD6

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -7,30 +7,30 @@ const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function getEvents() {
     const today = new Date().toISOString(); // Récupérer la date du jour. Regardez la doc de la méthode toISOString pour plus d'informations: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
-    let events = await pb.collection("events").getFullList(
+    let events = await pb.collection("evenement").getFullList(
         {
             sort: "-date",
             order: "desc",
             filter: `date >= "${today}"`,
         }
     ); // Filtrer les événements passés en fonction de la date. Si la colonne date n'est pas dans votre collection, vous pouvez la remplacer par une autre colonne de type date.
-    events = events.map((event) => {
-        event.img = pb.files.getURL(event, event.imgUrl);
-        return event;
+    evenement = evenement.map((evenement) => {
+        evenement.img = pb.files.getURL(evenement, evenement.imgUrl);
+        return evenement;
     }); // Récupérer l'URL de l'image de chaque événement. Vous pouvez remplacer imgUrl par le nom de la colonne qui contient le nom de l'image.
-    events = events.map((event) => {
-        event.date = formatDate(event.date);
-        return event;
+    evenement = evenement.map((evenement) => {
+        evenement.date = formatDate(evenement.date);
+        return evenement;
     }); // Formater la date de chaque événement. Vous pouvez remplacer date par le nom de la colonne qui contient la date de l'événement. La fonction formatDate est importée du fichier utils.mjs.
-    return events;
+    return evenement;
 }
 
 export async function getOneEvent(id) {
     try {
-        const event = await pb.collection("events").getOne(id);
-        event.img = pb.files.getURL(event, event.imgUrl);
-        event.formattedDate = formatDate(event.date);
-        return event;
+        const evenement = await pb.collection("evenement").getOne(id);
+        evenement.img = pb.files.getURL(evenement, evenement.imgUrl);
+        evenement.formattedDate = formatDate(evenement.date);
+        return evenement;
     } catch (error) {
         return null;
     }
@@ -40,7 +40,7 @@ export async function getOneEvent(id) {
 export async function setFavoriteEvent(id, valeurFavori) {
     try {
         console.log("id", id, "valeurFavori", valeurFavori);
-        await pb.collection("events").update(id, { favori: valeurFavori });
+        await pb.collection("evenement").update(id, { favori: valeurFavori });
         return true;
     } catch (error) {
         console.log("error", error);
@@ -50,7 +50,7 @@ export async function setFavoriteEvent(id, valeurFavori) {
 
 export async function addEvent(data) {
     try {
-        await pb.collection("events").create(data);
+        await pb.collection("evenement").create(data);
         return {
             success: true,
             message: "L'événement a été ajouté avec succès.",
@@ -65,22 +65,22 @@ export async function addEvent(data) {
 
 export async function filterByCategory(category) {
     try {
-        let events = await pb.collection("events").getFullList({
+        let evenement = await pb.collection("evenement").getFullList({
             filter: `categorie = "${category}"`,
         });
-        events = events.map((event) => {
-            event.img = pb.files.getURL(event, event.imgUrl);
-            return event;
+        evenements = evenement.map((evenement) => {
+            evenement.img = pb.files.getURL(evenement, evenement.imgUrl);
+            return evenement;
         });
         return {
             success: true,
-            events: events,
+            evenements: evenements,
             message: "Les événements ont été filtrés avec succès.",
         }
     } catch (error) {
         return {
             success: false,
-            events: [],
+            evenement: [],
             message: "Une erreur est survenue lors du filtrage des événements: " + error,
         }
     }
@@ -91,13 +91,13 @@ export async function updateEvent(id, data) {
         const event = await pb.collection("events").update(id, data);
         return {
             success: true,
-            event: event,
+            evenement: evenement,
             message: "L'événement a été modifié avec succès.",
         };
     } catch (error) {
         return {
             success: false,
-            event: null,
+            evenement: null,
             message: "Une erreur est survenue lors de la modification de l'événement: " + error,
         };
     }
